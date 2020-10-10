37197f89d97c504965dc544f4553de92ba6ab2a1
Buddha Man Nepali <bmnepali980@gmail.com>
2020-10-10 20:58:52 +0545

    R-4: Removes duplicate herders form campaign page

--- src/pages/Campaign.js
+++ src/pages/Campaign.js
@@ -15,30 +15,26 @@ const Campaign = (props) => {
 
   return (
     <Fragment>
-      <HeaderMain />
-      <div className="main-container">
-        <section className="filter-bar ">
-          <div className="inner-filter-bar w-100">
-            <div className="container">
-              <div className="row align-items-center">
-                <div className="col-md-6">
-                  <PageTitleCampaignDropdown />
-                </div>
-                <div className="col-md-6 text-right">
-                  <Link to="./https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" className="btn btn-link btn-download-report">
-                                        Download Report
-                  </Link>
-                </div>
+      <section className="filter-bar ">
+        <div className="inner-filter-bar w-100">
+          <div className="container">
+            <div className="row align-items-center">
+              <div className="col-md-6">
+                <PageTitleCampaignDropdown />
+              </div>
+              <div className="col-md-6 text-right">
+                <Link to="./https://www.w3.org/WAI/ER/tests/xhtml/testfiles/resources/pdf/dummy.pdf" className="btn btn-link btn-download-report">
+                  Download Report
+                </Link>
               </div>
             </div>
           </div>
-        </section>
-        <CampaignGraph campaignId = {id}/>
-        <TopCreatives />
-        <TopLandingPages />
-        <TopTargets />
-      </div>
-      <Footer />
+        </div>
+      </section>
+      <CampaignGraph campaignId = {id}/>
+      <TopCreatives />
+      <TopLandingPages />
+      <TopTargets />
     </Fragment>
   );
 };
