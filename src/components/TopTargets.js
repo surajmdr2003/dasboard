a089b4ce4f3437e39c8067b7f1fe5d62e34ed9a0
Buddha Man Nepali <bmnepali980@gmail.com>
2020-10-10 20:58:17 +0545

    R-4: Makes api call to load targetting data amd renders the map

--- src/components/TopTargets.js
+++ src/components/TopTargets.js
@@ -1,13 +1,41 @@
-import React from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
+import { Auth, API } from 'aws-amplify';
+import MapComponent from './MapComponent';
 
 const TopTargets = () => {
+  const apiRequest = {
+    headers: { accept: '*/*' },
+    response: true,
+    queryStringParameters: {},
+  };
+  const [isLoading, setIsLoading] = useState(false);
+  const [targets, setTargets] = useState([]);
+  const [selectedTarget, setSelectedTarget] = useState(null);
+
+  useEffect(() => {
+    setIsLoading(true);
+    Auth.currentSession()
+      .then(async function(info) {
+        const accessToken = info.getAccessToken().getJwtToken();
+
+        // Setting up header info
+        apiRequest.headers.authorization = `Bearer ${accessToken}`;
+        const response = await API.post('canpaignGroupTargeting', '', apiRequest);
+
+        // Updating the response to the state
+        setTargets(response.data);
+      })
+      .catch(() => false)
+      .finally(() => setIsLoading(false));
+  }, [])
+
   return (
     <section className="target-location-content">
       <div className="container">
         <div className="row align-items-center filter-block">
           <div className="col-md-5">
             <div className="block-title">
-                            Top Creatives
+              Top Targets
               <a href="#" className="btn-link">See All</a>
             </div>
           </div>
@@ -15,29 +43,29 @@ const TopTargets = () => {
             <button type="button" className="btn btn-outline-primary btn-date-picker">Last 30 days</button>
           </div>
         </div>
-
         <div className="card card-map">
           <div className="row location-container">
-            <div className="locations col-sm-3 pr-0">
-              <div className="card border-0">
-                <div className="card-header">
-                                    Target Locations
-                </div>
-                <ul className="list-group list-group-flush">
-                  <li className="list-group-item">Cras justo odio</li>
-                  <li className="list-group-item">Dapibus ac facilisis in</li>
-                  <li className="list-group-item">Vestibulum at eros</li>
-                  <li className="list-group-item">Hawthrone</li>
-                  <li className="list-group-item">Manhattan Beach</li>
-                  <li className="list-group-item">Hermosa Beach</li>
-                  <li className="list-group-item">Torrace</li>
-                  <li className="list-group-item">Rolling Hills Estates</li>
-                </ul>
-              </div>
-            </div>
-            <div className="map col-sm-9 pl-0">
-              <img src="/assets/images/map.png" alt="map" />
-            </div>
+            {
+              isLoading
+                ? <div className="col text-center m-5">
+                    <center><div className="spinner-grow spinner-grow-lg" role="status"> <span className="sr-only">Loading...</span></div></center>
+                  </div>
+                : <Fragment>
+                    <div className="locations col-sm-3 pr-0">
+                      <div className="card border-0">
+                        <div className="card-header">
+                          Target Locations
+                        </div>
+                        <ul className="list-group list-group-flush">
+                          { targets.map(target => <li key={target.id}  style={{cursor: 'pointer'}} className="list-group-item" onClick={(e) => {e.preventDefault(); setSelectedTarget(target)}}>{target.name}</li>)}
+                        </ul>
+                      </div>
+                    </div>
+                    <div className="col-sm-9 pl-0">
+                      <MapComponent target={selectedTarget ? selectedTarget : {data: null}}/>
+                    </div>
+                  </Fragment>
+            }
           </div>
         </div>
       </div>
