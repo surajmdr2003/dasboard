8673e877c464d9e5b62d3bc71c7299c55f2dfec2
Buddha Man Nepali <bmnepali980@gmail.com>
2020-10-17 17:49:21 +0545

    Moves the nav related components to navigation folder

---
+++ src/components/navigation/HeaderMain.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Logo from '../Logo';
+import Navigation from './Navigation';
+
+const HeaderMain = () => {
+  return (
+    <header className="main-header">
+      <nav className="navbar navbar-expand-lg">
+        <Logo />
+        <Navigation/>
+      </nav>
+    </header>
+  );
+};
+
+export default HeaderMain;
