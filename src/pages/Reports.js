da1fd4974dc38e40fdb0662200af6f3fd02cb114
suraj <surajmdr2003@gmail.com>
2020-10-23 16:06:51 +0545

    Adds functionality of modal on report

--- src/pages/Reports.js
+++ src/pages/Reports.js
@@ -1,6 +1,7 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 import DataTable from 'react-data-table-component';
 
 /** Services */
@@ -8,11 +9,13 @@ import ReportService from '../services/report.service';
 
 /** Components */
 import AlertComponent from '../components/AlertComponent';
+import ErrorMessage from '../components/common/ErrorMessage.component';
 import PageTitleCampaignDropdown from '../components/PageTitleCampaignDropdown';
 
 
 const Reports = (props) => {
   const campaignId = props.match.params.id;
+  const [isModalOpen, toggleModal] = useState(false);
   const [loading, setLoading] = useState(false);
   const [perPage, setPerPage] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
@@ -93,7 +96,7 @@ const Reports = (props) => {
     </ul>
   );
 
-  const  sendEmail = async(event, reportId) => {
+  const sendEmail = async (event, reportId) => {
     event.preventDefault();
     setEmailNotification({
       ...setEmailNotification,
@@ -125,7 +128,7 @@ const Reports = (props) => {
       });
   };
 
-  const fetchCampaignReports = async(page) => {
+  const fetchCampaignReports = async (page) => {
     setLoading(true);
     ReportService.getReports(campaignId, page, perPage)
       .then((response) => {
@@ -140,10 +143,13 @@ const Reports = (props) => {
     fetchCampaignReports(page);
   };
 
-  const handlePerRowsChange = async(newPerPage, page) => {
+  const handlePerRowsChange = async (newPerPage, page) => {
     setPerPage(newPerPage);
     fetchCampaignReports(page);
   };
+
+  const { register, handleSubmit, errors, isSubmitting } = useForm();
+  const onSubmit = data => console.log(data);
 
   return (
     <Fragment>
@@ -174,6 +180,53 @@ const Reports = (props) => {
             onChangeRowsPerPage={handlePerRowsChange}
             onChangePage={handlePageChange}
           />
+
+          <button className="btn btn-outline-primary" onClick={() => toggleModal(true)}>Show Modal</button>
+          <div className={`custom-modal ${(isModalOpen ? 'show' : 'hide')}`}>
+            <div className="modal-block">
+              <div className="modal-header">
+                <h4>Email this report</h4>
+                <span className="icon-close" onClick={() => toggleModal(false)}>
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 492 492">
+                    <path d="M300.188,246L484.14,62.04c5.06-5.064,7.852-11.82,7.86-19.024c0-7.208-2.792-13.972-7.86-19.028L468.02,7.872    c-5.068-5.076-11.824-7.856-19.036-7.856c-7.2,0-13.956,2.78-19.024,7.856L246.008,191.82L62.048,7.872    c-5.06-5.076-11.82-7.856-19.028-7.856c-7.2,0-13.96,2.78-19.02,7.856L7.872,23.988c-10.496,10.496-10.496,27.568,0,38.052    L191.828,246L7.872,429.952c-5.064,5.072-7.852,11.828-7.852,19.032c0,7.204,2.788,13.96,7.852,19.028l16.124,16.116    c5.06,5.072,11.824,7.856,19.02,7.856c7.208,0,13.968-2.784,19.028-7.856l183.96-183.952l183.952,183.952    c5.068,5.072,11.824,7.856,19.024,7.856h0.008c7.204,0,13.96-2.784,19.028-7.856l16.12-16.116    c5.06-5.064,7.852-11.824,7.852-19.028c0-7.204-2.792-13.96-7.852-19.028L300.188,246z" />
+                  </svg>
+                </span>
+              </div>
+              <div className="modal-body">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <div className="form-group">
+                    <label htmlFor="emailAddress" className="label">Email address</label>
+                    <input
+                      id="emailAddress"
+                      name="emailAddress"
+                      type="text"
+                      className="form-control"
+                      placeholder="you@example.com"
+                      autoFocus
+                      ref={register({
+                        required: 'Please enter email address.',
+                      })}
+                    />
+                    <ErrorMessage error={errors.emailAddress} />
+                  </div>
+                  <div className="form-group">
+                    <label htmlFor="message" className="label">Leave a message</label>
+                    <textarea
+                      rows="5"
+                      id="message"
+                      name="message"
+                      type="text"
+                      className="form-control"
+                      placeholder="Type your message here"
+                    />
+                  </div>
+                  <div className="form-group mb-0">
+                    <button disabled={isSubmitting} type="submit" className="mt-3 btn btn-default btn-primary">Email Report</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </Fragment>
