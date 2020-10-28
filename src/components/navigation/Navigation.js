f5c22c54b73bbfcd3e3e00dd8c43158cfb84c0a2
Buddha Man Nepali <bmnepali980@gmail.com>
2020-10-28 23:30:41 +0545

    R-11: Adds functionality to switch to other user as admin

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -15,23 +15,38 @@ import NavDropdownCreatives from './NavDropdownCreatives';
 import AdvertiserService from '../../services/advertiser.service';
 
 const Navigation = () => {
-  const {user, setActiveCampaign} = React.useContext(GlobalContext);
+  const {user, setUser, setActiveCampaign} = React.useContext(GlobalContext);
   const [campaignList, setCampaignList] = useState(window.$campaigns);
   const [avaliableUsers, setAvailableUsers] = useState([]);
   const [isSearching, setIsSearcing] = useState(false);
   const [isOpen, toggleDropdown] = useState(false);
+  const initCampaigns = {id: null, name: 'No Campaign Found'};
 
   const loadCampaignsData = () => {
     return AdvertiserService.getAdvertiserCampaignGroups(user.id)
       .then((response) => {
-        window.$campaigns = response.data;
+        window.$campaigns = response.data.length ? response.data : [];
         PubSub.publish('CAMPAIGNS:LOADED', response.data);
-        setActiveCampaign(response.data.length ? response.data[3] : { id: null});
+        setActiveCampaign(response.data.length ? response.data[0] : initCampaigns);
         setCampaignList(response.data);
       })
       .catch(() => console.log('No campaigns available for user: ' + user.id));
   };
 
+  /**
+   * Check User Permission for user switch
+   */
+  const checkPermission = () => {
+    return user ? user.permissions.includes('ROLE_USER') && user.permissions.includes('ROLE_ADMIN') : false;
+  };
+
+  // Debouncing the change event
+  const handleSearchAdvertisers = _debounce(query => searchAdvertisers(query), 500);
+
+  /**
+   * Searcning the Advertisers
+   * @param {String} searchQuery
+   */
   const searchAdvertisers = (searchQuery) => {
     setIsSearcing(true);
     AdvertiserService.searchAdvertisers(searchQuery)
@@ -42,8 +57,8 @@ const Navigation = () => {
   };
 
   useEffect(() => {
-    window.$campaigns.length === 0 && loadCampaignsData();
-  }, []);
+    loadCampaignsData();
+  }, [user.id]);
 
   return (
     <Fragment>
@@ -70,36 +85,45 @@ const Navigation = () => {
           <li><NavLink to="/dashboard/reports">Report</NavLink></li>
         </ul>
         <ul className="navbar-nav align-items-center secondary-menu">
-          <li>
-            <i className="icon icon-advertiser"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="18" viewBox="0 0 20 18">
-              <g fill="none" fillRule="evenodd">
-                <g fill="#A0A0A0" fillRule="nonzero">
-                  <path d="M1140.833 26.938v3.125c0 .517-.42.937-.937.937h-3.958c-.518 0-.938-.42-.938-.938v-3.125c0-.517.42-.937.938-.937h3.958c.518 0 .937.42.937.938zm7.084 9.375v-3.126c0-.517-.42-.937-.938-.937h-3.958c-.518 0-.938.42-.938.938v3.124c0 .518.42.938.938.938h3.958c.518 0 .938-.42.938-.938zm1.25-9.376v3.125c0 .518.42.938.937.938h3.958c.518 0 .938-.42.938-.938v-3.125c0-.517-.42-.937-.938-.937h-3.958c-.518 0-.937.42-.937.938zm-1.25 3.125v-3.125c0-.517-.42-.937-.938-.937h-3.958c-.518 0-.938.42-.938.938v3.125c0 .517.42.937.938.937h3.958c.518 0 .938-.42.938-.938zm-8.021 2.188h-3.958c-.518 0-.938.42-.938.938v3.124c0 .518.42.938.938.938h3.958c.518 0 .937-.42.937-.938v-3.124c0-.518-.42-.938-.937-.938zM1135 39.438v3.124c0 .518.42.938.938.938h3.958c.518 0 .937-.42.937-.938v-3.124c0-.518-.42-.938-.937-.938h-3.958c-.518 0-.938.42-.938.938zm15.104-2.188h3.958c.518 0 .938-.42.938-.938v-3.124c0-.518-.42-.938-.938-.938h-3.958c-.518 0-.937.42-.937.938v3.124c0 .518.42.938.937.938zm0 6.25h3.958c.518 0 .938-.42.938-.938v-3.124c0-.518-.42-.938-.938-.938h-3.958c-.518 0-.937.42-.937.938v3.124c0 .518.42.938.937.938zm-8.02-4.063v3.126c0 .517.419.937.937.937h3.958c.518 0 .938-.42.938-.938v-3.124c0-.518-.42-.938-.938-.938h-3.958c-.518 0-.938.42-.938.938z" transform="translate(-1135 -26)" />
-                </g>
-              </g>
-            </svg></i>
-            <div className="dropdown-menu advertiser-dropdown-menu">
-              <ul className="list-group advertiser">
-                <li className="list-group-item">
-                  <div className="search-advitiser">
-                    <input type="text" name="search" className="form-control" placeholder="Search advertiser by name…" onChange={(e) => _debounce(searchAdvertisers(e.target.value), 5000)}/>
-                    <button type="submit">
-                      {
-                        isSearching
-                          ? <div className="spinner-grow spinner-grow-sm" role="status"> <span className="sr-only">Loading...</span></div>
-                          : <svg xmlns="http://www.w3.org/2000/svg" width="15" height="16" viewBox="0 0 15 16">
-                            <g fill="#505050" fillRule="nonzero">
-                              <path d="M1381.722 105.187l-2.87-2.87c1.07-1.2 1.735-2.788 1.735-4.523 0-3.746-3.048-6.794-6.793-6.794-3.746 0-6.794 3.048-6.794 6.794 0 3.745 3.048 6.793 6.794 6.793 1.33 0 2.561-.389 3.6-1.037l2.983 2.983c.178.178.421.276.664.276.244 0 .487-.098.665-.276.39-.373.39-.973.016-1.346zm-12.825-7.393c0-2.692 2.189-4.897 4.897-4.897 2.707 0 4.88 2.205 4.88 4.897 0 2.691-2.189 4.896-4.88 4.896-2.692 0-4.897-2.189-4.897-4.896z" transform="translate(-1367 -91)" />
-                            </g>
-                          </svg>
-                      }
-                    </button>
-                  </div>
-                </li>
-                {avaliableUsers.map(advertiser =>  <li key={advertiser.id} className="list-group-item">{advertiser.name}</li>)}
-              </ul>
-            </div>
-          </li>
+          {
+            checkPermission()
+              ? <li>
+                <i className="icon icon-advertiser"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="18" viewBox="0 0 20 18">
+                  <g fill="none" fillRule="evenodd">
+                    <g fill="#A0A0A0" fillRule="nonzero">
+                      <path d="M1140.833 26.938v3.125c0 .517-.42.937-.937.937h-3.958c-.518 0-.938-.42-.938-.938v-3.125c0-.517.42-.937.938-.937h3.958c.518 0 .937.42.937.938zm7.084 9.375v-3.126c0-.517-.42-.937-.938-.937h-3.958c-.518 0-.938.42-.938.938v3.124c0 .518.42.938.938.938h3.958c.518 0 .938-.42.938-.938zm1.25-9.376v3.125c0 .518.42.938.937.938h3.958c.518 0 .938-.42.938-.938v-3.125c0-.517-.42-.937-.938-.937h-3.958c-.518 0-.937.42-.937.938zm-1.25 3.125v-3.125c0-.517-.42-.937-.938-.937h-3.958c-.518 0-.938.42-.938.938v3.125c0 .517.42.937.938.937h3.958c.518 0 .938-.42.938-.938zm-8.021 2.188h-3.958c-.518 0-.938.42-.938.938v3.124c0 .518.42.938.938.938h3.958c.518 0 .937-.42.937-.938v-3.124c0-.518-.42-.938-.937-.938zM1135 39.438v3.124c0 .518.42.938.938.938h3.958c.518 0 .937-.42.937-.938v-3.124c0-.518-.42-.938-.937-.938h-3.958c-.518 0-.938.42-.938.938zm15.104-2.188h3.958c.518 0 .938-.42.938-.938v-3.124c0-.518-.42-.938-.938-.938h-3.958c-.518 0-.937.42-.937.938v3.124c0 .518.42.938.937.938zm0 6.25h3.958c.518 0 .938-.42.938-.938v-3.124c0-.518-.42-.938-.938-.938h-3.958c-.518 0-.937.42-.937.938v3.124c0 .518.42.938.937.938zm-8.02-4.063v3.126c0 .517.419.937.937.937h3.958c.518 0 .938-.42.938-.938v-3.124c0-.518-.42-.938-.938-.938h-3.958c-.518 0-.938.42-.938.938z" transform="translate(-1135 -26)" />
+                    </g>
+                  </g>
+                </svg></i>
+                <div className="dropdown-menu advertiser-dropdown-menu">
+                  <ul className="list-group advertiser">
+                    <li className="list-group-item">
+                      <div className="search-advitiser">
+                        <input type="text" name="search" className="form-control" placeholder="Search advertiser by name…" onChange={(e) => handleSearchAdvertisers(e.target.value)}/>
+                        <button type="submit">
+                          {
+                            isSearching
+                              ? <div className="spinner-grow spinner-grow-sm" role="status"> <span className="sr-only">Loading...</span></div>
+                              : <svg xmlns="http://www.w3.org/2000/svg" width="15" height="16" viewBox="0 0 15 16">
+                                <g fill="#505050" fillRule="nonzero">
+                                  <path d="M1381.722 105.187l-2.87-2.87c1.07-1.2 1.735-2.788 1.735-4.523 0-3.746-3.048-6.794-6.793-6.794-3.746 0-6.794 3.048-6.794 6.794 0 3.745 3.048 6.793 6.794 6.793 1.33 0 2.561-.389 3.6-1.037l2.983 2.983c.178.178.421.276.664.276.244 0 .487-.098.665-.276.39-.373.39-.973.016-1.346zm-12.825-7.393c0-2.692 2.189-4.897 4.897-4.897 2.707 0 4.88 2.205 4.88 4.897 0 2.691-2.189 4.896-4.88 4.896-2.692 0-4.897-2.189-4.897-4.896z" transform="translate(-1367 -91)" />
+                                </g>
+                              </svg>
+                          }
+                        </button>
+                      </div>
+                    </li>
+                    {avaliableUsers.map(advertiser =>  (<li key={advertiser.id} className="list-group-item">
+                      <div className="row">
+                        <div className="col-6 text-left">{advertiser.name}</div>
+                        <div className="col-6 text-right"><button onClick={() => setUser({permissions: user.permissions, ...advertiser})} className="btn btn-primary btn-sm">Login</button></div>
+                      </div>
+                    </li>))}
+                  </ul>
+                </div>
+              </li>
+              : ''
+          }
           <li>
             <i className="icon icon-notification" />
             <div className="dropdown-menu notification-dropdown-menu">
