2e3450c29ff41c9049e7cafe7805676fe56c5b05
Buddha Man Nepali <bmnepali980@gmail.com>
2020-11-02 16:46:51 +0545

    R-14: Updates the zoom functionality with improvements

--- src/components/MapComponent.js
+++ src/components/MapComponent.js
@@ -13,7 +13,41 @@ class MapComponent extends PureComponent {
         lat: 40.730,
         lng: -73.935,
       },
+      zoomMap: {
+        '1000': 5,
+        '800': 7,
+        '500': 9,
+        '200': 11,
+      },
     };
+  }
+
+  getZoomLevel = (noOfCordinates) => {
+    let zoom = 8;
+
+    switch (true) {
+      case noOfCordinates > 1000:
+        zoom = this.state.zoomMap['1000'];
+        break;
+
+      case noOfCordinates > 800:
+        zoom = this.state.zoomMap['800'];
+        break;
+
+      case noOfCordinates > 500:
+        zoom = this.state.zoomMap['500'];
+        break;
+
+      case noOfCordinates > 200:
+        zoom = this.state.zoomMap['200'];
+        break;
+
+      default:
+        zoom = 8;
+        break;
+    }
+
+    return zoom;
   }
 
   render() {
@@ -33,7 +67,7 @@ class MapComponent extends PureComponent {
       }
 
       if (data.type === 'poly') {
-        zoom = data.coordinates.length > 400 ? 5 : 11;
+        zoom = this.getZoomLevel(data.coordinates.length);
         centerLatLang = data.coordinates.length ? data.coordinates[0] : {...this.state.latlng};
 
         drawings.push((
