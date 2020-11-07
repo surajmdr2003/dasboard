ad8dc133d05136f65627ffdfab89aabfca0781ce
Buddha Man Nepali <bmnepali980@gmail.com>
2020-11-07 18:29:44 +0545

    R-18: Create and optimise the C3 Chart component

---
+++ src/components/common/C3Chart.js
@@ -0,0 +1,37 @@
+import React, {useEffect} from 'react';
+import PropTypes from 'prop-types';
+import c3 from 'c3';
+
+const C3Chart = ({holder, columns, axis, size, bar, legend}) => {
+  useEffect(() => {
+    const options = {};
+
+    axis ? options.axis = axis : '';
+    size ? options.size = size : '';
+    bar ? options.bar = bar : '';
+    legend ? options.legend = legend : '';
+
+    c3.generate({
+      bindto: '#' + holder,
+      data: columns,
+      transition: {
+        duration: 100,
+      },
+      ...options,
+    });
+  }, [columns]);
+
+  return <div id={holder}/>;
+};
+
+C3Chart.propTypes = {
+  columns: PropTypes.any,
+  axis: PropTypes.object,
+  size: PropTypes.object,
+  bar: PropTypes.object,
+  legend: PropTypes.object,
+  holder: PropTypes.string,
+};
+
+export default C3Chart;
+
