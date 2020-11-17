2440e3d1c6e6532389c56110a443e405c7615d94
Buddha Man Nepali <bmnepali980@gmail.com>
2020-11-17 12:35:46 +0545

    R-24: Fixes issues in fetching conversions

--- src/pages/Creatives.js
+++ src/pages/Creatives.js
@@ -112,7 +112,7 @@ const Creatives = () => {
    */
   const prepareTableRow = (row) => {
     row.ctr = handleNanValueWithCalculation(row.clicks, row.impressions);
-    row.conversions = row.conversions.reduce((sum, next) => sum + next.count, 0).toLocaleString();
+    row.conversions = Array.isArray(row.conversions) ? row.conversions.reduce((sum, next) => sum + next.count, 0).toLocaleString() : row.conversions.toLocaleString();
     row.convRate = handleNanValueWithCalculation(+row.conversions, row.clicks);
 
     return row;
