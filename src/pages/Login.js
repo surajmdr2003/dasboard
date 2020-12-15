93d0448a1f331150edd2e0076895c3e24008e97f
Buddha Man Nepali <bmnepali980@gmail.com>
2020-12-15 22:28:22 +0545

    R-35: Updates the login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,6 @@
 import React, { useEffect, Fragment, useState } from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router-dom';
-import cogoToast from 'cogo-toast';
 
 // AWS Amplify Related
 import { Auth } from 'aws-amplify';
@@ -15,9 +14,12 @@ const loginBanner = '/assets/images/login-banner.jpg';
 const Login = (props) => {
   const [isLoading, setIsLoading] = useState(false);
 
+  /**
+   * Show Greeting Message
+   */
   const showGreetingMessage = () => {
-    const hrs = new Date().getHours();
     let greet;
+    const hrs = new Date().getHours();
 
     if (hrs < 12) {greet = 'Good Morning!';} else if (hrs >= 12 && hrs <= 17) {greet = 'Good Afternoon!';} else if (hrs >= 17 && hrs <= 24) {greet = 'Good Evening!';}
 
@@ -27,15 +29,21 @@ const Login = (props) => {
   useEffect(() => {
     setIsLoading(true);
     Auth.currentAuthenticatedUser()
-      .then(() => {
+      .then((response) => {
+        const permissions = response.signInUserSession.accessToken.payload['cognito:groups'];
+        const isAuthorized = permissions.includes('ROLE_USER') || permissions.includes('ROLE_ADMIN') || permissions.includes('ROLE_ADVERTISER');
+
         // Hide loader/spinner
         setIsLoading(false);
 
-        // Redirect user to Dashboard
-        props.history.push('/dashboard');
+        if (!isAuthorized) {
+          props.history.push('/unauthorized');
+        } else {
+          // Redirect user to Dashboard
+          props.history.push('/dashboard');
+        }
       })
       .catch(() => {
-        cogoToast.warn('User Not Signed In!', { position: 'bottom-left' });
         setIsLoading(false);
       });
   }, []);
