9210a006f521527f9182cd007f1101d79004ac43
Buddha Man Nepali <bmnepali980@gmail.com>
2020-12-15 10:23:32 +0545

    R-34: Updates the auth service to check for loggedin status

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,7 +1,15 @@
 import { Auth } from 'aws-amplify';
+import Storage from '../utilities/Storage';
 
 class AuthService {
+  isLoggedIn() {
+    Storage.getItem('CognitoIdentityServiceProvider.6bs2nga1t9t9aqbqmot3qqb91k.admin.idToken')
+      ? ''
+      : window.location.href = '/';
+  }
+
   async getSessionInfo() {
+    this.isLoggedIn();
     return await Auth.currentSession();
   }
 }
