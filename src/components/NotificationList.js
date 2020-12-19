2390e43d9efaf037c0d88975e26641f397a5c979
suraj <surajmdr2003@gmail.com>
2020-12-19 19:56:43 +0545

    Fixes minors issues plus changes datatable library for sorting icons on all pages

--- src/components/NotificationList.js
+++ src/components/NotificationList.js
@@ -10,7 +10,8 @@ import AdvertiserService from '../services/advertiser.service';
 import NotificationService from '../services/notification.service';
 
 const NavigationList = (props) => {
-  const {user} = React.useContext(GlobalContext);
+  const { user } = React.useContext(GlobalContext);
+  const [isLoading, setIsloading] = useState(false);
   const [notifications, setNotifications] = useState([]);
   const iconMap = {
     'IDEA': 'icon-bulb',
@@ -19,17 +20,22 @@ const NavigationList = (props) => {
   };
 
   const loadNotifications = () => {
+    setIsloading(true);
     return AdvertiserService.getAdvertiserNotifications(user.id)
       .then((response) => {
         setNotifications((props.displaySummary ? response.data.slice(0, 3) : response.data) || []);
+        setIsloading(false);
       })
-      .catch(() => cogoToast.error('No Notifications available for user: ' + user.id, {position: 'bottom-left'}));
+      .catch(() => {
+        cogoToast.error('No Notifications available for user: ' + user.id, { position: 'bottom-left' });
+        setIsloading(false);
+      });
   };
 
   const makeApiCall = (event, notificationId, actionType) => {
     event.preventDefault();
     let linkWindow;
-
+    setIsloading(true);
     NotificationService.getNotificationAction(notificationId)
       .then((response) => {
         if (actionType === 'STATIC_URL') {
@@ -39,10 +45,14 @@ const NavigationList = (props) => {
         }
 
         if (actionType === 'API_CALL') {
-          cogoToast.success(response.data.value, {position: 'bottom-center', hideAfter: 3});
+          cogoToast.success(response.data.value, { position: 'bottom-center', hideAfter: 3 });
         }
+        setIsloading(false);
       })
-      .catch(() => ('No Notifications available for user: ' + user.id));
+      .catch(() => {
+        ('No Notifications available for user: ' + user.id);
+        setIsloading(true);
+      });
   };
 
   const getActionTypeLabel = (notificationId, actionType, action) => {
@@ -56,28 +66,29 @@ const NavigationList = (props) => {
   return (
     <Fragment>
       {
-        notifications.length < 1
-          ? <li className="media">
-            <i className="icon-notification" />
-            <div className="media-body">
-              <h5>No Notifications available</h5>
-            </div>
-          </li>
-          : ''
-      }
-      {
-        notifications.map((notification) => (
-          <li key={notification.id} className="media">
-            <span className="icon-box mr-3">
-              <i className={iconMap[notification.icon]} />
-            </span>
-            <div className="media-body">
-              <h5>{notification.title}</h5>
-              <p>{notification.description}</p>
-              {getActionTypeLabel(notification.id, notification.actionType, notification.actionName)}
-            </div>
-          </li>
-        ))
+        isLoading
+          ? <div className="text-center m-5">
+            <div className="spinner-grow spinner-grow-lg" role="status"> <span className="sr-only">Loading...</span></div>
+          </div>
+          : notifications.length < 1
+            ? <li className="media">
+              <i className="icon-notification" />
+              <div className="media-body">
+                <h5>No Notifications available</h5>
+              </div>
+            </li>
+            : notifications.map((notification) => (
+              <li key={notification.id} className="media">
+                <span className="icon-box mr-3">
+                  <i className={iconMap[notification.icon]} />
+                </span>
+                <div className="media-body">
+                  <h5>{notification.title}</h5>
+                  <p>{notification.description}</p>
+                  {getActionTypeLabel(notification.id, notification.actionType, notification.actionName)}
+                </div>
+              </li>
+            ))
       }
     </Fragment>
   );
